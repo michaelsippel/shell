ca5ecee0a2f8201fada5559c1e530375399ee2c1
Michael Sippel <micha@fragmental.art>
2023-08-08 15:10:15 +0200

    gettype.sh: display current pattern in case of error by grep

--- gettype.sh
+++ gettype.sh
@@ -5,6 +5,10 @@ while read -r pattern
 do
     if echo "$2" | grep -q "^${pattern}$"; then
 	grep -A 2 -F "::${pattern}" $1 | grep -m 1 -Po "$3: \K.*$" && exit
+    else
+	if [ $? == 2 ]; then
+	   echo "^-- in pattern ${pattern}"
+	fi
     fi
 done
 
