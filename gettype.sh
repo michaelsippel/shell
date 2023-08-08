b86195b987ef110ee84122d346f7d63188b5cfef
Michael Sippel <micha@fragmental.art>
2023-08-08 16:33:06 +0200

    gettype.sh: print matching pattern on stderr
    typedb: fix find patterns & add more patterns

--- gettype.sh
+++ gettype.sh
@@ -3,11 +3,11 @@
 grep -Po '::\K.*$' $1 |
 while read -r pattern
 do
-    if echo "$2" | grep -q "^${pattern}$"; then
-	grep -A 2 -F "::${pattern}" $1 | grep -m 1 -Po "$3: \K.*$" && exit
+    if echo "$2" | grep -xq "${pattern}"; then
+	grep -A 2 -Fx "::${pattern}" $1 | grep -m 1 -Po "$3: \K.*$" &&  echo "^-- in pattern ${pattern}" 1>&2 && exit
     else
-	if [ $? == 2 ]; then
-	   echo "^-- in pattern ${pattern}"
+	if [ $? -eq 2 ]; then
+	   echo "^-- in pattern ${pattern}" 1>&2
 	fi
     fi
 done
