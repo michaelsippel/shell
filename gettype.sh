f8a63d6595972df49e85f54ade8d995e017272dc
Michael Sippel <micha@fragmental.art>
2023-08-08 15:09:53 +0200

    getttype.sh: pin pattern to begin of line

--- gettype.sh
+++ gettype.sh
@@ -3,7 +3,7 @@
 grep -Po '::\K.*$' $1 |
 while read -r pattern
 do
-    if echo "$2" | grep -q "${pattern}$"; then
+    if echo "$2" | grep -q "^${pattern}$"; then
 	grep -A 2 -F "::${pattern}" $1 | grep -m 1 -Po "$3: \K.*$" && exit
     fi
 done
